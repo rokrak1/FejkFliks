baf5711798c98a9a06d2094b484390c4c5c26cc4
rok <rok.rak1@gmail.com>
2024-04-23 01:06:52 +0200

    and its always good to stage all the changes :P

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://emperor.si/",
-});
+export default ({ mode }: { mode: any }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_PUBLIC_URL,
+  });
+};
